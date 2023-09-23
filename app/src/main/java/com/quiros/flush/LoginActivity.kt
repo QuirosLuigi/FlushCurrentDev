21c1ff76e950a77f477e2dd0b20fcc1365e7c802
ArnoldQuiros <arnoldlquiros@gmail.com>
2023-09-23 20:35:53 +0800

    Login and Register Backend Finished

--- app/src/main/java/com/quiros/flush/LoginActivity.kt
+++ app/src/main/java/com/quiros/flush/LoginActivity.kt
@@ -1,53 +1,81 @@
 package com.quiros.flush
 
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var usernameEditText: EditText
+    private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var registerButton : TextView
 
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+            //if (currentUser != null) {
+            //val intent = Intent(this, RegisterActivity::class.java)
+            //startActivity(intent)
+            //finish() // Close the LoginActivity
+        //}
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
-        usernameEditText = findViewById(R.id.email_input)
+        auth = Firebase.auth
+        emailEditText = findViewById(R.id.email_input)
         passwordEditText = findViewById(R.id.password_input)
         loginButton = findViewById(R.id.login_btn)
+        registerButton = findViewById(R.id.Signup2TV)
 
         // Set click listener for the login button
         loginButton.setOnClickListener {
             // Get the entered username and password
-            val username = usernameEditText.text.toString()
+            val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
 
             // Perform authentication (you should replace this with your actual authentication logic)
-            val isAuthenticated = authenticateUser(username, password)
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val intent = Intent(this, RegisterActivity::class.java)  // change to homepage
+                        startActivity(intent)
+                        finish() // Close the LoginActivity
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
 
-            if (isAuthenticated) {
-                // Authentication successful, navigate to the next activity
-                //val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                finish() // Close the LoginActivity
-            } else {
-                // Authentication failed, display a message to the user
-                Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
-            }
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish() // Close the LoginActivity
         }
     }
 
-    // Replace this with your actual authentication logic
-    private fun authenticateUser(username: String, password: String): Boolean {
-        // Example: Check if username and password match a valid user
-        return username == "user" && password == "password"
-    }
+
+
+
 }
--- app/src/main/java/com/quiros/flush/RegisterActivity.kt
+++ app/src/main/java/com/quiros/flush/RegisterActivity.kt
@@ -1,11 +1,75 @@
 package com.quiros.flush
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var cpasswordEditText: EditText
+    private lateinit var signupButton: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var loginbutton : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        auth = Firebase.auth
+        emailEditText = findViewById(R.id.EmailET)
+        passwordEditText = findViewById(R.id.PasswordET)
+        cpasswordEditText = findViewById(R.id.CPasswordET)
+        signupButton = findViewById(R.id.signup_btn)
+        loginbutton = findViewById(R.id.Login2TV)
+
+        signupButton.setOnClickListener {
+            Log.i("meow","tanginamu")
+            Log.i("meow", passwordEditText.text.toString())
+            Log.i("meow", cpasswordEditText.text.toString())
+            if(passwordEditText.text.toString() == cpasswordEditText.text.toString()){
+                auth.createUserWithEmailAndPassword(emailEditText.text.toString(),passwordEditText.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success")
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish() // Close the LoginActivity
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
+            }
+            else
+                Toast.makeText(
+                    baseContext,
+                    "Password and Confirm Password Doesn't Match",
+                    Toast.LENGTH_SHORT,
+                ).show()
+        }
+        loginbutton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Close the LoginActivity
+        }
+
+
+
     }
 }
